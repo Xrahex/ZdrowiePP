e1ee04ca65b81ec2cfc475575448273671597a88
Xrahex <qwertytyija@gmail.com>
2025-05-22 16:26:00 +0200

    Update CreateTrainingPlanActivity.java

--- app/src/main/java/com/example/zdrowiepp/CreateTrainingPlanActivity.java
+++ app/src/main/java/com/example/zdrowiepp/CreateTrainingPlanActivity.java
@@ -7,7 +7,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 import java.util.List;
